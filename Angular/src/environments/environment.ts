24fbfa7b41d08a5c7cc33f363cf0a168ec154d25
Chandan Kumar <chandankumar7995@gmail.com>
2019-11-26 09:30:51 +0530

    update

--- Angular/src/environments/environment.ts
+++ Angular/src/environments/environment.ts
@@ -4,11 +4,10 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:44384/api',
-  apiAuthServicesBaseUrl: 'https://localhost:44315/authservice',
-  apiAdminServicesBaseUrl: 'https://localhost:44315/adminservice',
-  apiMentorServicesBaseUrl: 'https://localhost:44315/mentorservice',
-  apiStudentServicesBaseUrl: 'https://localhost:44315/studentservice'
+  apiAuthServicesBaseUrl: 'http://localhost:9095/authservice',
+  apiAdminServicesBaseUrl: 'http://localhost:9095/adminservice',
+  apiMentorServicesBaseUrl: 'http://localhost:9095/mentorservice',
+  apiStudentServicesBaseUrl: 'http://localhost:9095/studentservice'
 };
 
 /*
